8c2464d0fed8365056130f4818d4f668c67dce3d
Tuan Ngoc <ngoc.mai@axonactive.com>
2018-11-22 14:28:44 +0700

    Change css for sign in button and change firebase prod config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
-  production: true,
+  production: false,
   firebase: {
     apiKey: 'AIzaSyDrh_HauM4PpUjuroREMbYjSXA9klvJp-s',
     authDomain: 'mp3-angular4.firebaseapp.com',
     databaseURL: 'https://mp3-angular4.firebaseio.com',
     projectId: 'mp3-angular4',
-    storageBucket: '',
+    storageBucket: 'mp3-angular4.appspot.com',
     messagingSenderId: '220425624941'
   }
 };
